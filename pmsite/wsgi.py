1217d8539e8911f2679e9bb156c0c82264bca82c
root <root@mail.jongedemocraten.nl>
2013-05-21 22:19:46 +0200

    Remove sys path thingies

--- pmsite/wsgi.py
+++ pmsite/wsgi.py
@@ -17,13 +17,6 @@ import os
 import sys
 import site
 
-site.addsitedir('/home/tom/pm/venv/lib/python2.7/site-packages')
-sys.path.insert(0, '/home/tom/pm/venv/lib/python2.7/site-packages')
-sys.path.insert(0, '/home/tom/pm/pmsite/pmsite')
-sys.path.insert(0, '/home/tom/pm/pmsite')
-#sys.path.insert(0, '/home/tom')
-
-
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
